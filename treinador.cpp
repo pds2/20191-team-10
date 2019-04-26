3d4d7ac50a7f5296530c904d9a82117bbc18f28e
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-04-26 09:17:07 -0300

    Create treinador.cpp

---
+++ treinador.cpp
@@ -0,0 +1,27 @@
+#include "treinador.h"
+#include "pokemon.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+Treinador::Treinador(std::string nome,int lideranca):
+	_jogador(nome), _lideranca(lideranca){};
+	
+std::string Treinador::get_jogador(){
+	return _jogador;
+}
+
+int Treinador::get_lideranca(){
+	return _lideranca;
+}
+
+void Treinador::set_lideranca(int x){
+	_lideranca=x;
+}
+
+void Treinador::add_pokemon(Pokemon novo_pokemon){
+	_lista_de_pokemons.push_back(novo_pokemon);
+}
+
+
