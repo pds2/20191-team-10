60ecf022e533ec93e53f9ab911eb0e4cadfc3cc4
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-22 10:49:37 -0300

    Update pokemon.cpp

--- src/pokemon.cpp
+++ src/pokemon.cpp
@@ -1,86 +1,29 @@
 #include "../include/pokemon.h"
 
-#include <iostream>
 #include <string>
+#include <iostream>
 
-Pokemon::Pokemon(std::string nome,float crit,int HP,char ataque,char defesa,char agilidade):
-	_nome(nome), _crit(crit), _HP(HP), _ataque(ataque), _defesa(defesa), _agilidade(agilidade){};
+Pokemon::Pokemon(std::string nome,char tipo,int ataque,int defesa,int agilidade ,int hp,int crit):
+	_nome(nome),_tipo(tipo),_ataque(ataque),_agilidade(agilidade),_hp(hp),_crit(crit),current_hp(hp){};
 
+char Pokemon::get_tipo(){
+    return this->_tipo;
+}
 int Pokemon::get_ataque(){
-	if(_ataque=='A'){
-		return 30;
-	}
-	else if(_ataque=='B'){
-		return 25;
-	}
-	else if(_ataque=='C'){
-		return 20;
-	}
-	else if(_ataque=='D'){
-		return 15;
-	}
-	else if(_ataque=='E'){
-		return 10;
-	}
-	else if(_ataque=='F'){
-		return 5;
-	}
+    return this->_ataque;
 }
-
-int Pokemon::get_agilidade(){
-	if(_agilidade=='A'){
-		return 6;
-	}
-	else if(_agilidade=='B'){
-		return 5;
-	}
-	else if(_agilidade=='C'){
-		return 4;
-	}
-	else if(_agilidade=='D'){
-		return 3;
-	}
-	else if(_agilidade=='E'){
-		return 2;
-	}
-	else if(_agilidade=='F'){
-		return 1;
-	}
-}
-
-int Pokemon::get_crit(){
-	return _crit;
-}
-
 int Pokemon::get_defesa(){
-	if(_ataque=='A'){
-		return 18;
-	}
-	else if(_ataque=='B'){
-		return 15;
-	}
-	else if(_ataque=='C'){
-		return 12;
-	}
-	else if(_ataque=='D'){
-		return 9;
-	}
-	else if(_ataque=='E'){
-		return 6;
-	}
-	else if(_ataque=='F'){
-		return 3;
-	}
+    return this->_defesa;
 }
-
-int Pokemon::get_HP(){
-	return _HP;
+int Pokemon::get_agilidade(){
+    return this->_agilidade;
 }
-
+int Pokemon::get_hp(){
+    return this->_hp;
+}
+int Pokemon::get_crit(){
+    return this->_crit;
+}
 std::string Pokemon::get_nome(){
-	return _nome;
-}
-
-int Pokemon::get_id(){
-	return _id;
+    return this->_nome;
 }
