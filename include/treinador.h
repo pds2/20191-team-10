8a24763f1bbd6bbccdc56ea229f0fbfe5db601dc
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-22 10:42:52 -0300

    Update treinador.h

--- include/treinador.h
+++ include/treinador.h
@@ -1,7 +1,8 @@
-#ifndef TREINADOR_H	
+#ifndef TREINADOR_H
 #define TREINADOR_H
 
 #include "pokemon.h"
+
 #include <string>
 #include <vector>
 
@@ -9,18 +10,18 @@ class Treinador{
 	protected:
 		std::string _jogador;
 	public:
-		
 		int _lideranca; // Atributo Liderança do Treinador deve ser alterado logo está no escopo público
-		
+
+		std::vector<Pokemon*> _lista_de_pokemons;
+
 		Treinador(std::string,int); // Construtor : Nome, Nível de lideranca
-		//~Treinador();
-		
-		std::vector<Pokemon> _lista_de_pokemons;
-		
-		std::string get_jogador();
+
+		// Métodos para manipulação do TAD :
+
 		int get_lideranca();
 		void set_lideranca(int);
-		void add_pokemon(Pokemon);
+		void add_pokemon(Pokemon*);
+		std::string get_jogador();
 };
 
 #endif
