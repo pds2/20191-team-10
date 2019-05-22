76e17d9c29fe44c45f53c4b0b31d7edd831e04b9
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-22 10:41:42 -0300

    Delete pokedex.h

--- include/pokedex.h
+++
@@ -1,6 +0,0 @@
-#include "pokemon.h"
-#include "treinador.h"
-
-Pokemon Bulbasauro = Pokemon("Bulbasauro",1.1,45,'D','D','C');
-Pokemon Charmander = Pokemon("Charmander",1.11,39,'C','D','E');
-Pokemon Squirtle = Pokemon("Squirtle",1.1,42,'D','C','E');
