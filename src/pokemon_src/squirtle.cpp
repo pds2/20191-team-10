af25cd6ad8fbf102275f60b3f246e13d45227119
leodpereira <dupeleonardo@gmail.com>
2019-05-24 12:14:54 -0300

    move pokemon src files to pokemon_src folder

---
+++ src/pokemon_src/squirtle.cpp
@@ -0,0 +1,20 @@
+#include "../include/pokemon_agua.h"
+#include "../include/pokemon_include/squirtle.h"
+
+#include <string>
+#include <iostream>
+
+Squirtle::Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
+	Pokemon_Agua(nome, ataque, defesa, agilidade, hp, crit){};
+
+Squirtle::Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
+	Pokemon_Agua(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia, imunidade){};
+
+// Métodos Polimórficos :
+
+void Squirtle::atacar(Pokemon *adv){
+    std::cout<<"Dummy Function"; //vamos utilizar os metodos get_fraqueza/get_resistencia/get_imunidade
+																//para determinar o ataque
+}
+
+// Habilidades....
