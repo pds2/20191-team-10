1ed56f617288e68f9aa79813288f43f9cfd21113
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-24 10:52:44 -0300

    Update pokemonAgua.cpp

--- src/pokemonAgua.cpp
+++ src/pokemonAgua.cpp
@@ -6,14 +6,14 @@ Pokemon_Agua::Pokemon_Agua(std::string nome, int ataque, int defesa, int agilida
 Pokemon_Agua::Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
   Pokemon(nome, ataque, defesa, agilidade, hp, crit), _fraqueza(fraqueza), _resistencia(resistencia), _imunidade(imunidade) {}
 
-std::vector<std::string> PokemonAgua::get_fraqueza() {
+std::vector<std::string> Pokemon_Agua::get_fraqueza() {
   return this->_fraqueza;
 }
 
-std::vector<std::string> PokemonAgua::get_resistencia() {
+std::vector<std::string> Pokemon_Agua::get_resistencia() {
   return this->_resistencia;
 }
 
-std::vector<std::string> PokemonAgua::get_imunidade() {
+std::vector<std::string> Pokemon_Agua::get_imunidade() {
   return this->_imunidade;
 }
