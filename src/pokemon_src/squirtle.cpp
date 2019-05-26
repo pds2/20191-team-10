792aadbdc8ae8e6e9ef463ea25cec080bb32a1da
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:14:39 -0300

    Update squirtle.cpp

--- src/pokemon_src/squirtle.cpp
+++ src/pokemon_src/squirtle.cpp
@@ -6,8 +6,8 @@
 Squirtle::Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
 	Pokemon_Agua(nome, ataque, defesa, agilidade, hp, crit){};
 
-Squirtle::Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
-	Pokemon_Agua(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia, imunidade){};
+Squirtle::Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia):
+	Pokemon_Agua(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia){};
 
 // Métodos Polimórficos :
 
