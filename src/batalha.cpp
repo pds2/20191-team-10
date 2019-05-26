602052d116014cddc1176e16c1b7566bf5ca40f3
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 12:09:14 -0300

    Update batalha.cpp

--- src/batalha.cpp
+++ src/batalha.cpp
@@ -1,5 +1,8 @@
 #include "../include/batalha.h"
+#include "../include/excecoes.h"
 
+#include <stdexcept>
+#include <exception>
 #include <ctime>
 #include <cstdlib>
 
@@ -8,4 +11,13 @@ int rola_dados(){
 	int temp = (rand()%10) + 1;
 
 	return(temp);
+}
+
+void encerrar_batalha(Pokemon *poke){
+    try{
+        excpt_verificar_morto(poke);
+    }
+    catch(std::out_of_range &e){
+        poke->current_hp=poke->get_hp();
+    }
 }
