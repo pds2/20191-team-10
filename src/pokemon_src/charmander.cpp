37c4ce12d9b0f984a0f11af3a5ee964b2ce4353c
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:14:25 -0300

    Update charmander.cpp

--- src/pokemon_src/charmander.cpp
+++ src/pokemon_src/charmander.cpp
@@ -6,8 +6,8 @@
 Charmander::Charmander(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
 	Pokemon_Fogo(nome, ataque, defesa, agilidade, hp, crit){};
 
-Charmander::Charmander(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
-	Pokemon_Fogo(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia, imunidade){};
+Charmander::Charmander(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia):
+	Pokemon_Fogo(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia){};
 
 // Métodos Polimórficos :
 
