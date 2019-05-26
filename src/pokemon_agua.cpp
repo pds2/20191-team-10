11cbccb73687d5ea64f3cd0cc9e7725fa63a9723
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:15:13 -0300

    Update pokemon_agua.cpp

--- src/pokemon_agua.cpp
+++ src/pokemon_agua.cpp
@@ -1,19 +1,20 @@
 #include "../include/pokemon_agua.h"
 
 Pokemon_Agua::Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
-  Pokemon(nome, ataque, defesa, agilidade, hp, crit) {}
+  Pokemon(nome, ataque, defesa, agilidade, hp, crit) {};
 
-Pokemon_Agua::Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
-  Pokemon(nome, ataque, defesa, agilidade, hp, crit), _fraqueza(fraqueza), _resistencia(resistencia), _imunidade(imunidade) {}
+Pokemon_Agua::Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia):
+  Pokemon(nome, ataque, defesa, agilidade, hp, crit), _fraqueza(fraqueza), _resistencia(resistencia) {}
 
-std::vector<std::string> Pokemon_Agua::get_fraqueza() {
+std::string Pokemon_Agua::get_fraqueza() {
   return this->_fraqueza;
 }
 
-std::vector<std::string> Pokemon_Agua::get_resistencia() {
+std::string Pokemon_Agua::get_resistencia() {
   return this->_resistencia;
 }
-
+/*
 std::vector<std::string> Pokemon_Agua::get_imunidade() {
   return this->_imunidade;
 }
+*/
