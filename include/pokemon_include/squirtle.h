f948a71108b3fb6f9efde6f67a84d7ab349ebac1
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:16:44 -0300

    Update squirtle.h

--- include/pokemon_include/squirtle.h
+++ include/pokemon_include/squirtle.h
@@ -7,7 +7,7 @@
 
 class Squirtle : public Pokemon_Agua{
 	public:
-		Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade);
+		Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia);
 		Squirtle(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit);
 
 		virtual void atacar(Pokemon *) override;
