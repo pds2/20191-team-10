56d957320f7a1fb1507a39b3817ca02c720f1aa7
mathelho <48322185+mathelho@users.noreply.github.com>
2019-05-26 17:08:18 -0300

    Create introducao.h
    Esboço da introdução do jogo.

---
+++ include/introducao.h
@@ -0,0 +1,7 @@
+#ifndef INTRODUCAO_H
+#define INTRODUCAO_H
+
+void introducao(); // Texto introdutório do jogo
+void escolher_modo(); // Escolher modo batalha 1X1, 3X3 ou torneio
+
+#endif // INTRODUCAO_H
