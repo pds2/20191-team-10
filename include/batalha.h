30e75345c38f5c151739d95e052164d9427ba142
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 11:41:34 -0300

    Update batalha.h

--- include/batalha.h
+++ include/batalha.h
@@ -3,8 +3,6 @@
 
 #include "pokemon.h"
 
-void verificar_morto(Pokemon *); // Verifica se o HP do pokemon é <=0. Caso seja gera uma exceção ou trata essa exceção
-
 int rola_dados(); // Gera um número aleatório entre 1 e 10. Utilizado para calcular a chance de crit, por exemplo.
 
 void encerrar_batalha(Pokemon *, Pokemon *); // Encerra. Deve inclusive resetar o current_hp para o valor original.
