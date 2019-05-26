bae058c8be830c8c7f6699ca5d3b821f72d4d2b2
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:15:38 -0300

    Update pokemon_fogo.cpp

--- src/pokemon_fogo.cpp
+++ src/pokemon_fogo.cpp
@@ -1,19 +1,20 @@
 #include "../include/pokemon_fogo.h"
 
 Pokemon_Fogo::Pokemon_Fogo(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
-  Pokemon(nome, ataque, defesa, agilidade, hp, crit) {}
+  Pokemon(nome, ataque, defesa, agilidade, hp, crit) {};
 
-Pokemon_Fogo::Pokemon_Fogo(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
-  Pokemon(nome, ataque, defesa, agilidade, hp, crit), _fraqueza(fraqueza), _resistencia(resistencia), _imunidade(imunidade) {}
+Pokemon_Fogo::Pokemon_Fogo(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia):
+  Pokemon(nome, ataque, defesa, agilidade, hp, crit), _fraqueza(fraqueza), _resistencia(resistencia) {};
 
-std::vector<std::string> Pokemon_Fogo::get_fraqueza() {
+std::string Pokemon_Fogo::get_fraqueza() {
   return this->_fraqueza;
 }
 
-std::vector<std::string> Pokemon_Fogo::get_resistencia() {
+std::string Pokemon_Fogo::get_resistencia() {
   return this->_resistencia;
 }
-
+/*
 std::vector<std::string> Pokemon_Fogo::get_imunidade() {
   return this->_imunidade;
 }
+*/
