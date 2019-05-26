cd896d0b33c0696dd0e8d77142c93f5d37bfe617
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:17:17 -0300

    Update pokemon_agua.h

--- include/pokemon_agua.h
+++ include/pokemon_agua.h
@@ -3,19 +3,21 @@
 
 #include "pokemon.h"
 
+#include <string>
+
 class Pokemon_Agua : public Pokemon {
 protected:
-  std::vector<std::string> _fraqueza = {"grama", "eletrico"};
-  std::vector<std::string> _resistencia = {"fogo", "agua"};
-  std::vector<std::string> _imunidade = {""};
+  std::string _fraqueza = {"grama"};
+  std::string _resistencia = {"agua"};
+  //std::vector<std::string> _imunidade = {""};
 
 public:
   Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit);
-  Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade);
+  Pokemon_Agua(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia);
 
-  std::vector<std::string> get_fraqueza();
-  std::vector<std::string> get_resistencia();
-  std::vector<std::string> get_imunidade();
+  std::string get_fraqueza();
+  std::string get_resistencia();
+  //std::vector<std::string> get_imunidade();
 
 };
 
