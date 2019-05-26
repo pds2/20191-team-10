d71fd3c7d0c4e4bf69f590659bfca19ce9caf802
LeandroMvSouza <48248990+LeandroMvSouza@users.noreply.github.com>
2019-05-26 15:14:14 -0300

    Update bulbasauro.cpp

--- src/pokemon_src/bulbasauro.cpp
+++ src/pokemon_src/bulbasauro.cpp
@@ -6,8 +6,8 @@
 Bulbasauro::Bulbasauro(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit):
 	Pokemon_Grama(nome, ataque, defesa, agilidade, hp, crit){};
 
-Bulbasauro::Bulbasauro(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::vector<std::string> fraqueza, std::vector<std::string> resistencia, std::vector<std::string> imunidade):
-	Pokemon_Grama(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia, imunidade){};
+Bulbasauro::Bulbasauro(std::string nome, int ataque, int defesa, int agilidade, int hp, int crit, std::string fraqueza, std::string resistencia):
+	Pokemon_Grama(nome, ataque, defesa, agilidade, hp, crit, fraqueza, resistencia){};
 
 // Métodos Polimórficos :
 
