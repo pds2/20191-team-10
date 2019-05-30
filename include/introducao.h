52af2b0617a715abb1e00615ae4b5f61c4f0b2c8
mathelho <48322185+mathelho@users.noreply.github.com>
2019-05-29 22:38:51 -0300

    Update introducao.h

--- include/introducao.h
+++ include/introducao.h
@@ -1,7 +1,16 @@
 #ifndef INTRODUCAO_H
 #define INTRODUCAO_H
+#include "../include/introducao.h"
+#include "../include/treinador.h"
+#include "../include/pokemon_include/squirtle.h"
+#include "../include/pokemon_agua.h"
+#include "../include/pokemon_include/charmander.h"
+#include "../include/pokemon_include/bulbasauro.h"
+#include "../include/pokemon_fogo.h"
+#include "../include/pokemon_grama.h"
+#include "../include/batalha.h"
 
-void introducao(Treinador jogador); // Texto introdutório do jogo
-void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade); // Escolher modo batalha 1X1, 3X3 ou torneio
+void introducao(Treinador *jogador, Squirtle *aguin, Charmander *foguin, Bulbasauro *verdin); // Texto introdutório do jogo
+//void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade); // Escolher modo batalha 1X1, 3X3 ou torneio
 
 #endif // INTRODUCAO_H
