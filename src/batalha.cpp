974748430b2fd6f17d8d1f03034076c104c5a8a2
leodpereira <dupeleonardo@gmail.com>
2019-05-30 14:14:14 -0300

    remove escolher_pokemon function from batalha.cpp and update makefile

--- src/batalha.cpp
+++ src/batalha.cpp
@@ -49,30 +49,6 @@ void encerrar_batalha(Pokemon *meu_poke, Pokemon *inimigo){
     std::cout << "Vida do inimigo: " << inimigo->current_hp << std::endl;
 }*/
 
-int escolher_pokemon(Treinador jogador){
-    std::cout << "Qual Poke'mon tu vai querer usar na batalha?";
-    std::cout << std::endl;
-    jogador.print_lista_pokemon();
-    std::string escolha, confirmacao;
-
-
- //Criar uma exceção aqui depois & pensar numa maneira de o código não crashar caso tenha apenas 2
-
-
-    do{
-        do{
-            std::cout << "Digite o numero correspondente: ";
-            std::cin >> escolha;
-        }while((escolha[0] != '1') && (escolha[0] != '2') && (escolha[0] != '3'));
-        std::cout << "Tem certeza que quer usar ";
-        std::cout << jogador._lista_de_pokemon.at((escolha[0] - '0')-1)->get_apelido() << "? ";
-        std::cin >> confirmacao;
-        if(confirmacao[0] != 'S' && confirmacao[0] != 's')
-            std::cout << std::endl << "Qual Poke'mon quer levar? ";
-    }while((confirmacao[0] != 's') && (confirmacao[0] != 'S'));
-    return (escolha[0] - '0') - 1;
-}
-
 void batalha_x1(Treinador jogador, Pokemon *meu_poke, int dificuldade){
     Pokemon *inimigo;
     /*if(dificuldade== 1){
