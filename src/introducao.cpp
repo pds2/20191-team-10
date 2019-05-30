ec3dab0ae2451205bb190d872cf18a308b3b1050
mathelho <48322185+mathelho@users.noreply.github.com>
2019-05-29 22:40:02 -0300

    Update introducao.cpp

--- src/introducao.cpp
+++ src/introducao.cpp
@@ -2,46 +2,31 @@
 #include <string>
 #include <vector>
 #include "../include/introducao.h"
-#include "../include/treinador.h"
-#include "../include/pokemon_include/squirtle.h"
-#include "../include/pokemon_agua.h"
-#include "../include/pokemon_include/charmander.h"
-#include "../include/pokemon_include/bulbasauro.h"
-#include "../include/pokemon_fogo.h"
-#include "../include/pokemon_grama.h"
-#include "../include/batalha.h"
 
-Squirtle aguin = Squirtle("aguin",15,10,10,50,5);
-Charmander foguin = Charmander("foguin",15,10,10,50,5);
-Bulbasauro verdin = Bulbasauro("verdin",15,10,10,50,5);
+//Squirtle aguin = Squirtle("aguin",15,10,10,50,5);
+//Charmander foguin = Charmander("foguin",15,10,10,50,5);
+//Bulbasauro verdin = Bulbasauro("verdin",15,10,10,50,5);
 
-void introducao(Treinador jogador) {
+void introducao(Treinador *jogador, Squirtle *aguin, Charmander *foguin, Bulbasauro *verdin) {
     int p;
-    std::string nome;
-
-    std::cout << "Bem-vindo ao mundo Pokemon!" << std::endl;
-    std::cout << "Qual será o seu nome de treinador?" << std::endl;
-
-    std::getline(std::cin, nome);
-    jogador = Treinador(nome, 1);
 
     std::cout << "Pokemons para escolher: Bulbasauro, Squirtle e Charmander." << std::endl;
     std::cout << "Digite 1 para Bulbasauro, 2 para Squirtle e 3 para Charmander." << std::endl;
     std::cin >> p;
     switch(p) {
         case 1:
-            jogador.add_pokemon(&verdin);
+            jogador->add_pokemon(verdin);
             break;
         case 2:
-            jogador.add_pokemon(&aguin);
+            jogador->add_pokemon(aguin);
             break;
         case 3:
-            jogador.add_pokemon(&foguin);
+            jogador->add_pokemon(foguin);
             break;
     }
 }
 
-void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade) {
+/*void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade) {
     int p;
     if (jogador._lista_de_pokemons.size() < 3) {
         std::cout << "Digite 1 para modo 1X1. (Os outros modos são liberados ao ter 3 pokémons na equipe)" << std::endl;
@@ -65,4 +50,4 @@ void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade) {
                 break;
         }
     }
-}
+}*/
