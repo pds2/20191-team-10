809fef2e772154a77bb7a87e4beb5da44ce5eedd
leodpereira <dupeleonardo@gmail.com>
2019-06-05 07:44:01 -0300

    add pokebola header and src files

---
+++ include/pokebola.h
@@ -0,0 +1,14 @@
+#ifndef POKEBOLA_H
+#define POKEBOLA_H
+
+#include "pokemon.h"
+#include "treinador.h"
+#include "../include/pokemon_include/charmander.h"
+#include "../include/pokemon_include/bulbasauro.h"
+#include "../include/pokemon_include/squirtle.h"
+
+void capturar_pokemon(Treinador jogador, Pokemon *inimigo);
+// Função para capturar pokemon utilizando as pokebolas implementadas no treinador
+// Recebe um treinador como parametro e o pokemon inimigo ativo na batalha.
+
+#endif
