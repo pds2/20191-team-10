89bab60399d7f0db0e5c7e1408be3cc76cda51d1
leodpereira <dupeleonardo@gmail.com>
2019-06-06 11:28:33 -0300

    update pokedex

--- src/pokedex.cpp
+++ src/pokedex.cpp
@@ -4,10 +4,22 @@ void treinador_info(Treinador jogador) {
   std::cout << "----------------------------------------------------------------------------" << '\n';
   std::cout << "\t\t\tPOKÉDEX" << '\n';
   std::cout << "----------------------------------------------------------------------------" << '\n';
-  std::cout << "Treinador " << jogador.get_treinador() << '\n';
+  std::cout << "Treinador " << jogador.get_treinador();
+  std::cout << " - Liderança: " << jogador.get_lideranca() << "\n\n";
+  if(jogador.get_pokeball() > POKEBOLA_INVALIDO)
+    std::cout << "Pokeballs: " << jogador.get_pokeball() << '\n';
+  if(jogador.get_greatball() > POKEBOLA_INVALIDO)
+    std::cout << "Greatballs: " << jogador.get_greatball() << '\n';
+  if(jogador.get_masterball() > POKEBOLA_INVALIDO)
+    std::cout << "Masterballs: " << jogador.get_masterball() << '\n';
+  if(jogador.get_capturas_totais() > 0) {
+    std::cout << "\n\nTaxa de sucesso na captura de pokemons: " << jogador.get_taxa_sucesso() << '\n';
+  } else {
+    std::cout << "\n\nVocê ainda não tentou capturar pokemons, por isso sua taxa de sucesso na captura\
+    de pokemons está indisponível." << '\n';
+  }
   std::cout << "----------------------------------------------------------------------------" << '\n';
-  std::cout << "Pokemons: " << '\n';
-  std::cout << "________________________________________________" << '\n';
+  std::cout << "Pokemons:\n\n";
 
   for (auto& it : jogador._lista_de_pokemon) {
     std::cout << it->get_nome() << '\n';
