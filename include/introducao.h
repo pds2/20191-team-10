90426db65456340affb02639bd2234f937d9c36f
mathelho <matheus.coelho789@gmail.com>
2019-06-07 12:17:14 -0300

    Update introducao.h

--- include/introducao.h
+++ include/introducao.h
@@ -9,9 +9,18 @@
 #include "../include/pokemon_fogo.h"
 #include "../include/pokemon_grama.h"
 #include "../include/batalha.h"
+#include <stdexcept>
+
+class Excpt_Entrada_Inicial : public std::exception {
+public:
+    const char *what() {
+        return "Entrada invalida! Digite 1, 2 ou 3!\n";
+    }
+};
 
 std::string apelida_jogador();
 void introducao(Treinador *jogador); // Texto introdutório do jogo
+void verifica_entrada(int p);
 //void escolher_modo(Treinador jogador, Pokemon *meu_poke, int dificuldade); // Escolher modo batalha 1X1, 3X3 ou torneio
 
 #endif // INTRODUCAO_H
